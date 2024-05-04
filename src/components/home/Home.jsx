dcd78d34ca4fc92991be70c811fc3602c6416c22
suryatejathodupunuri <suryatejathodupunuri@gmail.com>
2024-05-04 17:20:27 +0530

    may 4th commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,38 +1,8 @@
-import React from 'react';
-import Transliteration from '../transliteration/Transliteration';
-import Inputdropdown from '../transliteration/Inputdropdown';
-import Outputdropdown from '../transliteration/Outputdropdown';
-import Button from '../transliteration/Button';
-import FileUpload from '../transliteration/FileUpload';
-import InputTextarea from '../transliteration/InputTextarea';
-import OutputTextarea from '../transliteration/OutputTextarea';
+import React from "react";
+import Transliteration from "../transliteration/Transliteration";
 
 const Home = () => {
-
-  return (
-    <div>
-      <Transliteration />
-      <div className="flex justify-between">
-        <div className="w-1/4">
-          <FileUpload />
-        </div>
-        <div className="w-1/4">
-          <Inputdropdown />
-        </div>
-        <div className="w-1/4">
-          <Outputdropdown />
-        </div>
-        <div className="w-1/4">
-          <Button />
-        </div>
-      </div>
-      <br />
-      <div className="flex gap-4">
-        <InputTextarea />
-        <OutputTextarea />
-      </div>
-    </div>
-  );
+  return <Transliteration />;
 };
 
 export default Home;
--- src/components/transliteration/Transliteration.jsx
+++ src/components/transliteration/Transliteration.jsx
@@ -1,16 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Transliteration = () => {
+  const [file, setFile] = useState(null);
+  const [fileContent, setFileContent] = useState("");
+  const [responseData, setResponseData] = useState("");
+
+  const handleFileChange = (e) => {
+   
+     const selectedFile = e.target.files[0];
+     setFile(selectedFile);
+ 
+     const reader = new FileReader();
+     reader.onload = (event) => {
+       setFileContent(event.target.result);
+     };
+     reader.readAsText(selectedFile);
+  };
+  
+  const handleSubmit = async () => {
+    try {
+       const response = await fetch('http://localhost:3001/uppercase', {
+         method: 'POST', // Specifies that this is a POST request
+         headers: {
+           'Content-Type': 'application/json', // Indicates that the body is JSON
+         },
+         body: JSON.stringify({ data: fileContent }), // The data to be sent in the request body
+       });
+       const data = await response.json();
+       setResponseData(data.data); // Update the state with the response data
+    } catch (error) {
+       console.error("Error:", error);
+    }
+   };
+
   return (
     <>
-      <div className="text-center" style={{ padding: "10px" }}>
-        <b className="mb-2 mt-0 text-3xl font-large leading-tight text-primary text-justify">
+      <div className="text-center mb-8">
+        <h1 className="mb-2 mt-0 text-3xl font-large leading-tight text-primary text-center">
           Transliteration for Indian Languages
-        </b>
+        </h1>
         <h5 className="mb-2 mt-0 text-s font-small leading-tight text-primary">
           Online Transliteration - Choose a file/Copy Paste text, Select
           Conversion type and submit.
         </h5>
+      </div>
+
+      <div className="flex flex-row space-x-4 mb-4">
+        <label
+          htmlFor="file-upload"
+          className="cursor-pointer bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Choose File
+        </label>
+        <input
+        id="file-upload"
+        type="file"
+        className="hidden"
+        onChange={handleFileChange}
+      />
+      {file && <p className="mt-0">Selected file: {file.name}</p>}
+
+        <label htmlFor="InputLanguage" className="mr-2">
+          Choose Input Language:
+        </label>
+        <select name="InputLanguage" id="InputLanguage" className="mr-2">
+          <option value="English">English</option>
+          <option value="Telugu">Telugu</option>
+          <option value="Hindi">Hindi</option>
+          <option value="Urdu">Urdu</option>
+          <option value="Tamil">Tamil</option>
+          <option value="Malyalam">Malyalam</option>
+        </select>
+        <label htmlFor="OutputLanguage" className="mr-2">
+          Choose Output Language:
+        </label>
+        <select name="OutputLanguage" id="OutputLanguage" className="mr-2">
+          <option value="Telugu">Telugu</option>
+          <option value="English">English</option>
+          <option value="Hindi">Hindi</option>
+          <option value="Urdu">Urdu</option>
+          <option value="Tamil">Tamil</option>
+          <option value="Malyalam">Malyalam</option>
+        </select>
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </div>
+      </div>
+      <div className="flex flex-row items-center justify-center space-x-4 mb-4">
+      <textarea
+        className="h-60 w-1/2 border-black border rounded border-double p-4"
+        value={fileContent}
+        onChange={(e) => setFileContent(e.target.value)}
+
+      />
+
+      <textarea
+        className="h-60 w-1/2 border-black border rounded border-double p-4"
+        value={responseData}
+        onChange={(e) => setResponseData(e.target.value)}
+
+      />
+
+      </div>
+      <div>
+        <button
+          className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          onClick={""}
+        >
+          Sample 1
+        </button>
       </div>
     </>
   );
