535118dc33dcf9ba7a015467f351af1ce725f07c
suryatejathodupunuri <suryatejathodupunuri@gmail.com>
2024-05-17 14:18:55 +0530

    may 17 commit

--- src/components/transliteration/Transliteration.jsx
+++ src/components/transliteration/Transliteration.jsx
@@ -5,7 +5,8 @@ const Transliteration = () => {
   const [file, setFile] = useState(null);
   const [fileContent, setFileContent] = useState("");
   const [responseData, setResponseData] = useState("");
-
+  const [inputLang, setInputLang] = useState("");
+  const [outputLang, setOutputLang] = useState("");
   const handleFileChange = (e) => {
     const selectedFile = e.target.files[0];
     setFile(selectedFile);
@@ -18,7 +19,12 @@ const Transliteration = () => {
   };
 
   const handleSubmit = async () => {
+    if (!inputLang || !outputLang) {
+      alert("Please select both source and target languages.");
+      return;
+    }
     try {
+
       const response = await fetch(
         "https://meity-auth.ulcacontrib.org/ulca/apis/v0/model/compute",
         {
@@ -39,6 +45,8 @@ const Transliteration = () => {
       const target = data.output[0].target;
       setResponseData(target);
       console.log(data);
+      console.log( inputLang);
+    console.log(outputLang);
     } catch (error) {
       console.error("Error:", error);
     }
@@ -46,23 +54,40 @@ const Transliteration = () => {
 
   return (
     <>
-    
-    <div class="max-w-xl px-4">
-    <label class="flex justify-center w-50 h-24 px-2 transition bg-white rounded-md appearance-none cursor-pointer hover:bg-gray-100 focus:outline-none shadow-md hover:shadow-lg">
-        <span class="flex items-center space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+      <div class="max-w-xl px-4">
+        <label class="flex justify-center w-50 h-24 px-2 transition bg-white rounded-md appearance-none cursor-pointer hover:bg-gray-100 focus:outline-none shadow-md hover:shadow-lg">
+          <span class="flex items-center space-x-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="w-6 h-6 text-gray-600"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+              />
             </svg>
-            <span class="font-medium text-gray-600">
-                Drop files to Attach (only .txt files)
-                <span class="text-blue-600 underline"></span>
-            </span>
-        </span>
-        <input type="file" name="file_upload" class="hidden" onChange={handleFileChange}></input>
-    </label>
-</div>
+            
+    <div className="text-center">
+      <span className="font-medium text-gray-600">
+        Click to upload or Drag & drop <br />
+        <span className="text-black-600 text-sm ">.txt files only</span>
+      </span>
+    </div>
 
-
+          </span>
+          <input
+            type="file"
+            name="file_upload"
+            class="hidden"
+            onChange={handleFileChange}
+          ></input>
+        </label>
+      </div>
 
       <div className="flex flex-col h-screen">
         <div className="px-10 flex items-center justify-center bg-gray-200 p-4 shadow-md rounded-lg relative">
@@ -72,13 +97,15 @@ const Transliteration = () => {
                 name="InputLanguage"
                 id="InputLanguage"
                 className="bg-white border border-gray-300 rounded-lg shadow-sm hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent pl-8 pr-4 py-2"
-              >
-                <option value="English">English</option>
-                <option value="Telugu">Telugu</option>
-                <option value="Hindi">Hindi</option>
-                <option value="Urdu">Urdu</option>
-                <option value="Tamil">Tamil</option>
-                <option value="Malayalam">Malayalam</option>
+                value={inputLang}
+                onChange={(e) => setInputLang(e.target.value)}>
+                  <option disabled value="">Source</option>
+                <option value="eng">English</option>
+                <option value="tel">Telugu</option>
+                <option value="hin">Hindi</option>
+                <option value="urd">Urdu</option>
+                <option value="tam">Tamil</option>
+                <option value="mal">Malayalam</option>
               </select>
               <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
                 <svg
@@ -103,13 +130,16 @@ const Transliteration = () => {
                 name="OutputLanguage"
                 id="OutputLanguage"
                 className="bg-white border border-gray-300 rounded-lg shadow-sm hover:border-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent pl-8 pr-4 py-2"
-              >
-                <option value="Telugu">Telugu</option>
-                <option value="English">English</option>
-                <option value="Hindi">Hindi</option>
-                <option value="Urdu">Urdu</option>
-                <option value="Tamil">Tamil</option>
-                <option value="Malayalam">Malayalam</option>
+              
+                value={outputLang}
+                onChange={(e) => setOutputLang(e.target.value)}>
+                  <option disabled value="">Target</option>
+                <option value="hin">Hindi</option>
+                <option value="eng">English</option>
+                <option value="tel">Telugu</option>
+                <option value="urd">Urdu</option>
+                <option value="tam">Tamil</option>
+                <option value="mal">Malayalam</option>
               </select>
               <div className="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
                 <svg
